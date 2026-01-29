7134b0fb527bd03f31c9a3bbd9fb2f9f06669fd2
SHAMMAS <muhdshammas612@gmail.com>
2026-01-29 12:41:51 +0530

    Update settings for production: disable debug mode and allow all hosts; add Procfile for deployment; include requirements for necessary packages.

--- UniMart/settings.py
+++ UniMart/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t55g9ur2_w2@)kf(lrqt4$#embilt3$g&=7-a#k)%1)6of)2yi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
